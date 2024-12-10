7361f10df47ebaf26df77d9d6702a0b0867bd968
stefanybeacides <stefanybeacides@hotmail.com>
2024-12-10 16:45:04 -0300

    Criação de ambiente

---
+++ src/test/java/com/project/cadAcad/CadAcadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cadAcad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadAcadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
